13e5955338f9b9c4af4d1abb16c500fcdd2bfaea
SafteyDav3 <talktomedw@gmail.com>
2022-03-29 01:18:52 -0500

    Adding items works!

--- assets/js/sand.js
+++ assets/js/sand.js
@@ -3,6 +3,10 @@ var itemCounter = 5;
 var addItem = function () {
   itemCounter++;
   console.log(itemCounter);
+  var newItem = document.createElement("li");
+  newItem = document.createTextNode("Item " + itemCounter);
+  console.log(newItem);
+  document.getElementById("main-list").appendChild(newItem);
 };
 
 var buttonClicked = document
