9c31424bc48886759e10de60a47017f201fdae2d
SafteyDav3 <talktomedw@gmail.com>
2022-03-29 00:48:11 -0500

    Broken and i don't have any clue why.

--- assets/js/sand.js
+++ assets/js/sand.js
@@ -1,7 +1,13 @@
 var itemCounter = 5;
 
+var addItem = function () {
+  itemCounter++;
+  var newItem = document.createElement("li");
+  newItem.innerHTML = "Item " + itemCounter;
+  console.log(itemCounter);
+  document.getElementById("ul").appendChild(newItem);
+};
+
 var buttonClicked = document
   .getElementById("add-item")
-  .addEventListener("click", function () {
-    console.log("Button clicked!");
-  });
+  .addEventListener("click", addItem);
