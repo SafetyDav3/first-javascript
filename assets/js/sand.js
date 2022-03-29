5b0594bf660208933d99e0e176e851db76ca77e9
SafteyDav3 <talktomedw@gmail.com>
2022-03-29 02:11:15 -0500

    Working button to add list items || New list items take on CSS

--- assets/js/sand.js
+++ assets/js/sand.js
@@ -1,14 +1,15 @@
 var itemCounter = 5;
 
 var addItem = function () {
-  itemCounter++;
-  console.log(itemCounter);
-  var newItem = document.createElement("li");
-  newItem = document.createTextNode("Item " + itemCounter);
-  console.log(newItem);
-  document.getElementById("main-list").appendChild(newItem);
+    itemCounter++;
+    console.log(itemCounter);
+    var newItem = document.createElement("li");
+    newItem.className = "the-items"
+    newItem.innerText = "Item " + itemCounter;
+    console.log(newItem);
+    document.getElementById("main-list").appendChild(newItem);
 };
 
 var buttonClicked = document
-  .getElementById("add-item")
-  .addEventListener("click", addItem);
+    .getElementById("add-item")
+    .addEventListener("click", addItem);
