fbac2afeef33d42c7952ea0b0b2ee0f120421187
SafteyDav3 <talktomedw@gmail.com>
2022-03-29 01:02:35 -0500

    Basic button fuctionality back.

--- assets/js/sand.js
+++ assets/js/sand.js
@@ -2,10 +2,7 @@ var itemCounter = 5;
 
 var addItem = function () {
   itemCounter++;
-  var newItem = document.createElement("li");
-  newItem.innerHTML = "Item " + itemCounter;
   console.log(itemCounter);
-  document.getElementById("ul").appendChild(newItem);
 };
 
 var buttonClicked = document
